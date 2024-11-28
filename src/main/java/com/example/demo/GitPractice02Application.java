1d3f42816e64764e03c19e2eb7b1a142a803dc0b
DESKTOP-8N3GG4O\GGG <klk510a@gmail.com>
2024-11-28 17:36:14 +0900

    프로젝트2 환경변수 완료

---
+++ src/main/java/com/example/demo/GitPractice02Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractice02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractice02Application.class, args);
+	}
+
+}
